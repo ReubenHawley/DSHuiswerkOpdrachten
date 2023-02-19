ea305faa1260af82fd2ad2f5b024c7673ec55066
Reuben <reubenhawley@protonmail.com>
2023-02-19 15:51:57 +0100

    split workloads into crisp-dm methodology

--- Helper_funcs.py
+++ Helper_funcs.py
@@ -1,3 +1,4 @@
+import numpy as np
 import pandas
 import pandas as pd
 import matplotlib as plt
@@ -39,5 +40,31 @@ def calculate_tukey(data_series) -> pd.tseries:
     IQR = Q3 - Q1
     lower_bound = Q1 - 1.5 * IQR
     upper_bound = Q3 + 1.5 * IQR
-    return [lower_bound,IQR,upper_bound]
-    # outliers = df[(df['TG'] < lower_bound) | (df['TG'] > upper_bound)]
+    return [lower_bound, IQR, upper_bound]
+
+def unfinished_function(df):
+    Average_temp = df["T_gem"].sort_values()
+    size = len(df)
+    Q1_index = int(size / 4)
+    Q2_index = int(size / 2)
+    Q3_index = int(size - Q1_index)
+
+    TF_averageQ1 = Average_temp.iloc[:Q1_index]
+    TF_averageQ2 = Average_temp.iloc[Q1_index:Q2_index]
+    TF_averageQ3 = Average_temp.iloc[Q2_index:Q3_index]
+    TF_averageQ4 = Average_temp.iloc[Q3_index:]
+
+    maxQ1 = max(TF_averageQ1)
+    maxQ3 = max(TF_averageQ3)
+
+    IQR = (maxQ3 - maxQ1)
+    quantiles = df.quantile([.25, .5, .75], axis = 0).reset_index()
+
+def detect_outliers_tukey(x, k=1.5):
+    q1 = np.percentile(x, 25)
+    q3 = np.percentile(x, 75)
+    iqr = q3 - q1
+    fence_low = q1 - k * iqr
+    fence_high = q3 + k * iqr
+    outliers = (x < fence_low) | (x > fence_high)
+    return outliers
